728789ce5c88fcddc0321234428008e870289ef2
aatalib <70227699+aatalib@users.noreply.github.com>
2020-12-12 23:08:24 -0500

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,22 @@
 youtuber = "Ali Talib"
 # A few way ways to do it
+<<<<<<< Updated upstream
 print("subscribe to " + youtuber)
 print("subscribe to {}".format(youtuber))
 print(f"subscribe to {youtuber}")
+=======
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+
+print(madlib)
+>>>>>>> Stashed changes
