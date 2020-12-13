19781639530e675316e799388b8a37886fdcbc23
aatalib <70227699+aatalib@users.noreply.github.com>
2020-12-12 22:58:58 -0500

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -2,4 +2,4 @@ youtuber = "Ali Talib"
 # A few way ways to do it
 print("subscribe to " + youtuber)
 print("subscribe to {}".format(youtuber))
-print(f"subscribe {youtuber}")
+print(f"subscribe to {youtuber}")
